b391f3159a80ad3c89269d411157ae89df99f49b
Ravi Kanodia <ravileonkanodia@gmail.com>
2017-05-26 17:22:29 -0700

    :sparkles: A bunch more stuff

--- js/s2map.js
+++ js/s2map.js
@@ -1,6 +1,7 @@
 var _ = require('underscore');
-
 var Long = require('long');
+var React = require('react');
+var ReactDOM = require('react-dom');
 var S2Map = {};
 
 // Email from Tye specified 'left-cornering-hard', 'right-cornering-hard',
@@ -15,16 +16,6 @@ var dangerousEvent = {
 };
 
 S2Map.init = function() {
-  var florida = {lat: 29, lng: -82};
-  var sv = {lat: 37.4, lng: -122};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 10,
-    center: sv
-  });
-  var marker = new google.maps.Marker({
-    position: sv,
-    map: map
-  });
 
   S2Map.map = map;
 
@@ -38,16 +29,16 @@ S2Map.init = function() {
     function(devicePath, deviceId) {
       var devicePathCoords =
         _.map(
-          _.sortBy(
             _.filter(
               devicePath,
               // 1000000000000001 is a valid S2 cell id, but in this data set
               // it is used to represent an "unknown" location (e.g. due to a
               // poor GPS signal)
-              function(message) { return message.event[2] != "1000000000000001"; }
+              function(message) { 
+                return message.event[4] == 'vehicle-state' && 
+                  message.event[2] != "1000000000000001";
+              }
             ),
-            function(message) { return message.event[3].video_start; }
-          ),
           S2Map.getLatLngForMessage);
       var devicePolyline = new google.maps.Polyline({
         path: devicePathCoords,
@@ -56,26 +47,61 @@ S2Map.init = function() {
         strokeOpacity: 1.0,
         strokeWeight: 2,
         map: S2Map.map
+      //  map: null
       });
       return devicePolyline;
     }
   );
 
+  window.DangerousEventSample = _.groupBy(
+    _.filter(
+      window.Sample.data,
+      message => (message.event[2] != "1000000000000001" && 
+      _.contains(_.keys(dangerousEvent), message.event[4]))
+    ),
+    function(message) {
+      return message.event[4];
+    });
+  
   // Tye's email describes the message format as
   // [message_id, device_id, s2_cell_id, message_params], but it's actually
   // [message_id, device_id, s2_cell_id, message_params, event_type]
-  window.DangerousEventMarker = _.map(
-    _.filter(
-      window.Sample.data,
-      message => _.contains(_.keys(dangerousEvent), message.event[4])
-    ),
-   // window.Sample.data,
-    message => [message.event[4], new google.maps.Marker({
-      position: S2Map.getLatLngForMessage(message),
-      map: S2Map.map,
-      label: dangerousEvent[message.event[4]].label
-    })]
+  window.DangerousEventCombinedRegion = {};
+  _.each(
+    window.DangerousEventSample,
+    function(dangerousEvents, eventType) {
+      var combinedRegions = {};
+      _.each(
+        dangerousEvents,
+        function(message) {
+          var id = Long.fromString(message.event[2], true, 16).toString();
+          var key = S2Geometry.S2.idToKey(id);
+          var combinedKey = key.slice(0, 20) + "10";
+          if (combinedRegions[combinedKey]) {
+            return;
+          }
+          var corners = S2Geometry.S2.S2Cell.FromHilbertQuadKey(combinedKey).getCornerLatLngs();
+          corners[4] = corners[0];
+          console.log("corners: " + corners);
+          var combinedRegion = new google.maps.Polygon({
+            paths: corners,
+            strokeColor: dangerousEvent[message.event[4]].color,
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: dangerousEvent[message.event[4]].color,
+            fillOpacity: .2,
+            map: S2Map.map
+          });
+          combinedRegions[combinedKey] = {
+            corners: corners,
+            region: combinedRegion
+          };
+        }
+      );
+      window.DangerousEventCombinedRegion[eventType] = combinedRegions;
+    }
   );
+
 
 };
 
@@ -83,16 +109,13 @@ S2Map.init = function() {
 S2Map.getLatLngForMessage = function(message) {
   var id = Long.fromString(message.event[2], true, 16).toString();
   var key = S2Geometry.S2.idToKey(id);
-  //console.log("raw id: " + message.event[2] + ", converted: " + id + "; key: " + key);
   return S2Geometry.S2.keyToLatLng(key);
 };
 
-S2Map.drawPathByDriverId = function(driver) {
-  var driverMessages = window.DriverSample[driver];
-  if (!driverMessages) {
-
-  }
-
+S2Map.getCornersForMessage = function(message) {
+  var id = Long.fromString(message.event[2], true, 16).toString();
+  var key = S2Geometry.S2.idToKey(id);
+  return S2Geometry.S2.S2Cell.FromHilbertQuadKey(key).getCornerLatLngs();
 };
 
 
--- target/app.js
+++ target/app.js
@@ -4,6 +4,17 @@ var _ = require('underscore');
 var Long = require('long');
 var S2Map = {};
 
+// Email from Tye specified 'left-cornering-hard', 'right-cornering-hard',
+// and 'acceleration-hard', but the actual data set uses 'corner-left-hard'
+// and 'corner-right-hard', while acceleration is not mentioned.
+var dangerousEvent = {
+  'acceleration-hard': { color: '#FF0000', label: 'A' },
+  'braking-hard': { color: '#FF00FF', label: 'B' },
+  'corner-left-hard': { color: '#FFFF00', label: 'L' },
+  'corner-right-hard': { color: '#00FFFF', label: 'R' },
+  'severe-g-event': { color: '#0000FF', label: 'G' }
+};
+
 S2Map.init = function() {
   var florida = {lat: 29, lng: -82};
   var sv = {lat: 37.4, lng: -122};
@@ -28,36 +39,111 @@ S2Map.init = function() {
     function(devicePath, deviceId) {
       var devicePathCoords =
         _.map(
-          _.sortBy(
+         // _.sortBy(
             _.filter(
               devicePath,
-              function(message) { return message.event[2] != "1000000000000001"; }
+              // 1000000000000001 is a valid S2 cell id, but in this data set
+              // it is used to represent an "unknown" location (e.g. due to a
+              // poor GPS signal)
+              function(message) { 
+                return message.event[4] == 'vehicle-state' && 
+                  message.event[2] != "1000000000000001";
+              }
             ),
-            function(message) { return message.event[3].video_start; }
-          ),
+            //function(message) { return message.event[3].video_start; }
+     //       function(message) { return message.event[3].start_time; }
+     //     ),
           S2Map.getLatLngForMessage);
       var devicePolyline = new google.maps.Polyline({
         path: devicePathCoords,
         geodesic: true,
         strokeColor: '#FF0000',
         strokeOpacity: 1.0,
-        strokeWeight: 2
+        strokeWeight: 2,
+        map: S2Map.map
+      //  map: null
       });
       return devicePolyline;
     }
   );
 
-  for (var i in window.DevicePath) {
-    window.DevicePath[i].setMap(S2Map.map);
-  }
+  window.DangerousEventSample = _.groupBy(
+    _.filter(
+      window.Sample.data,
+      message => (message.event[2] != "1000000000000001" && 
+      _.contains(_.keys(dangerousEvent), message.event[4]))
+    ),
+    function(message) {
+      return message.event[4];
+    });
+  
+  // Tye's email describes the message format as
+  // [message_id, device_id, s2_cell_id, message_params], but it's actually
+  // [message_id, device_id, s2_cell_id, message_params, event_type]
+  /*window.DangerousEventMarker = _.map(
+    _.filter(
+      window.Sample.data,
+      message => _.contains(_.keys(dangerousEvent), message.event[4])
+    ),
+    message => [message.event[4], new google.maps.Marker({
+      position: S2Map.getLatLngForMessage(message),
+      //map: S2Map.map,
+      label: dangerousEvent[message.event[4]].label
+    })]
+  );*/
+
+  window.DangerousEventCombinedRegion = {};
+  _.each(
+    window.DangerousEventSample,
+    function(dangerousEvents, eventType) {
+      var combinedRegions = {};
+      _.each(
+        dangerousEvents,
+        function(message) {
+          var id = Long.fromString(message.event[2], true, 16).toString();
+          var key = S2Geometry.S2.idToKey(id);
+          var combinedKey = key.slice(0, 20) + "10";
+          if (combinedRegions[combinedKey]) {
+            return;
+          }
+          var corners = S2Geometry.S2.S2Cell.FromHilbertQuadKey(combinedKey).getCornerLatLngs();
+          corners[4] = corners[0];
+          console.log("corners: " + corners);
+          var combinedRegion = new google.maps.Polygon({
+            paths: corners,
+            strokeColor: dangerousEvent[message.event[4]].color,
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: dangerousEvent[message.event[4]].color,
+            fillOpacity: .2,
+            map: S2Map.map
+          });
+          combinedRegions[combinedKey] = {
+            corners: corners,
+            region: combinedRegion
+          };
+        }
+      );
+      window.DangerousEventCombinedRegion[eventType] = combinedRegions;
+    }
+  );
+
 
 };
+
 
 S2Map.getLatLngForMessage = function(message) {
   var id = Long.fromString(message.event[2], true, 16).toString();
   var key = S2Geometry.S2.idToKey(id);
   //console.log("raw id: " + message.event[2] + ", converted: " + id + "; key: " + key);
   return S2Geometry.S2.keyToLatLng(key);
+};
+
+S2Map.getCornersForMessage = function(message) {
+  var id = Long.fromString(message.event[2], true, 16).toString();
+  var key = S2Geometry.S2.idToKey(id);
+  console.log("key: " + key);
+  return S2Geometry.S2.S2Cell.FromHilbertQuadKey(key).getCornerLatLngs();
 };
 
 S2Map.drawPathByDriverId = function(driver) {
@@ -67,7 +153,6 @@ S2Map.drawPathByDriverId = function(driver) {
   }
 
 };
-
 
 window.S2Map = S2Map;
 window.S2Geometry = require('s2-geometry');
