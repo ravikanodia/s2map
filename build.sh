f810010eb8b60b37f698e4fccba3806a4033f310
Ravi Kanodia <ravileonkanodia@gmail.com>
2017-05-28 00:07:04 -0700

    :construction: Build now builds to root directory

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-browserify -e js/s2map.js -o target/app.js
-cp index.html target/index.html
-cp s2map.css target/s2map.css
-cp js/sample.js target/sample.js
+browserify -e js/s2map.js -o app.js
